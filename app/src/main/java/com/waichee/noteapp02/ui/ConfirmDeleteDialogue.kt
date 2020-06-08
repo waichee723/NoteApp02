771cc4413f2d17f5a3c060ec8f5eda88cc5eb386
christina <waichee723@gmail.com>
2020-06-08 22:55:25 +0800

    Test commit

---
+++ app/src/main/java/com/waichee/noteapp02/ui/ConfirmDeleteDialogue.kt
@@ -0,0 +1,11 @@
+package com.waichee.noteapp02.ui
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.waichee.noteapp02.R
+
+class ConfirmDeleteDialogue: DialogFragment() {
+
+}
